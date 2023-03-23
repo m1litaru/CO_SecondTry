ef1cf99f64e6f971749fab3d2c1022736fe3ed6e
Militaru Adrian <adrian.militaru@avaelgo.ro>
2023-03-23 19:10:42 +0200

    Added getDays - method to compute date diff

--- main/MainClass.java
+++ main/MainClass.java
@@ -1,13 +1,24 @@
 package main;
 
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public class MainClass {
 
     int age;
     float y;
     String Gigi;
 
+    Date dateOfBirth;
+
+    Date dateOfHirement;
+
     public void showAgeOfGigi() {
         System.out.println(Gigi + " " + age);
+    }
+
+    public int getDays() {
+        return Math.toIntExact(TimeUnit.DAYS.convert(Math.abs(dateOfHirement.getTime() - dateOfBirth.getTime()), TimeUnit.MILLISECONDS));
     }
 
     String secondString;
